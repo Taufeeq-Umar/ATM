304e568be5b7cc5200bb9dfce972e2962431cad9
Taufeeq-Umar <153849926+Taufeeq-Umar@users.noreply.github.com>
2024-03-31 18:02:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000; //Dollars
+let myPin = 1234;
+let pinAnswres = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswres.pin === myPin) {
+    console.log("Correct pin code !!!");
+    let OperationAns = await inquirer.prompt([
+        {
+            name: "Operation",
+            message: "Please select option",
+            type: "list",
+            choices: ["Withdraw", "Check Balance"]
+        }
+    ]);
+    if (OperationAns.Operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("Your remaining balace is " + myBalance);
+    }
+    else if (OperationAns.Operation === "Check Balance") {
+        console.log("Your balace is  " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect pin Code");
+}
